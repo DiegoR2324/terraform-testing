0a19f898f2773def33d045d1f5eb78cbc09e3015
EC2 Default User <ec2-user@ip-172-31-83-78.ec2.internal>
2024-10-08 00:57:58 +0000

    agregar creacion de security group para hacer parte 2 de ejercicios

--- main.tf
+++ main.tf
@@ -7,7 +7,26 @@ resource "aws_instance" "despliegue-terraform-ec2" {
   instance_type          = "t2.micro"
   key_name               = "vockey"
   tags = {
-    Name      = "terraform-test"
+    Name      = "terra-part2"
     Terraform = "true"		
   }
  }
+
+resource "aws_security_group" "test_terraform_sg" {
+  name = "test_terraform_sg"
+  vpc_id = "vpc-049a46e013597a243"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.test_terraform_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.test_terraform_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
